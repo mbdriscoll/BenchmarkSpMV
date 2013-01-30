9053e766694888d5c5a061fd528256e82301d9e6
Michael Driscoll <mbdriscoll@gmail.com>
2013-01-30 15:32:05 -0800

    reformat table printer

--- driver.cpp
+++ driver.cpp
@@ -195,11 +195,14 @@ int main(int argc, char* argv[]) {
 
     printf("Platform  Time         Gflop/s     %%peak Gbyte/s      %%peak\n");
     printf("MKL      % 1.8f  % 2.8f  %02.f   %02.8f   %02.f\n", cpuRefTime,
-            gflop/cpuRefTime, 100.0*gflop/cpuRefTime/2.67 , gbytes/cpuRefTime, 100.0*gbytes/cpuRefTime/25.6);
+            gflop/cpuRefTime, 100.0*gflop/cpuRefTime/2.67,
+            gbytes/cpuRefTime, 100.0*gbytes/cpuRefTime/25.6);
     printf("cuSPARSE % 1.8f  % 2.8f  %02.f   %02.8f   %02.f\n", gpuRefTime,
-            gflop/gpuRefTime, 100.0*gflop/gpuRefTime/1345.0 , gbytes/gpuRefTime, 100.0*gbytes/gpuRefTime/177.4);
+            gflop/gpuRefTime, 100.0*gflop/gpuRefTime/1345.0,
+            gbytes/gpuRefTime, 100.0*gbytes/gpuRefTime/177.4);
     printf("Custom   % 1.8f  % 2.8f  %02.f   %02.8f   %02.f\n", myRefTime,
-            gflop/myRefTime, 100.0*gflop/myRefTime/1345.0 , gbytes/myRefTime, 100.8*gbytes/myRefTime/177.4);
+            gflop/myRefTime, 100.0*gflop/myRefTime/1345.0,
+            gbytes/myRefTime, 100.8*gbytes/myRefTime/177.4);
 
     return 0;
 }
