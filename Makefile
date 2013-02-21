8e751a0f4cc2b761c70edf229b6e6d24ac2ba078
Michael Driscoll <mbdriscoll@gmail.com>
2013-02-21 11:40:54 -0800

    put all logic in one c file

--- Makefile
+++ Makefile
@@ -1,14 +1,13 @@
 CC = icc
-CXX = icc
 
-CXXFLAGS = -O2 -openmp -I$(MKLROOT)/include -offload-option,mic,compiler,"  -L$(MKLROOT)/lib/mic -lmkl_intel_lp64 -lmkl_intel_thread -lmkl_core" -offload-attribute-target=mic 
+CFLAGS = -std=c99 -O2 -openmp -I$(MKLROOT)/include -offload-option,mic,compiler,"  -L$(MKLROOT)/lib/mic -lmkl_intel_lp64 -lmkl_intel_thread -lmkl_core" -offload-attribute-target=mic 
 LDFLAGS =  -lmkl_intel_lp64 -lmkl_intel_thread -lmkl_core -lpthread -lm 
 
-default: driver
-driver: driver.cpp cpu.cpp mic.cpp mmio.c
-	$(CXX) $^ -o $@ $(CXXFLAGS) $(LDFLAGS)
+default: bench 
+
+bench: mmio.o
 
 .PHONY: clean
 
 clean:
-	rm -rf driver *.o
+	rm -rf bench *.o
---
+++ bench.c
@@ -0,0 +1,152 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <float.h>
+
+#include <sys/time.h>
+
+#include <mkl_spblas.h>
+
+#include "mmio.h"
+
+#define NITER 1000
+
+#define ALLOC alloc_if(1) free_if(0)
+#define FREE  alloc_if(0) free_if(1)
+#define REUSE alloc_if(0) free_if(0)
+#define TEMP  alloc_if(1) free_if(1)
+
+void
+mm_read(char* filename, int *m, int *n, int *nnz, int **rowptrs, int **colinds, float **vals) {
+    FILE* mmfile = fopen(filename, "r");
+    assert(mmfile != NULL && "Read matrix file.");
+
+    int status;
+    MM_typecode matcode;
+    status = mm_read_banner(mmfile, &matcode);
+    assert(status == 0 && "Parsed banner.");
+
+    assert( mm_is_matrix(matcode) );
+    assert( mm_is_sparse(matcode) );
+    assert( mm_is_real  (matcode) );
+
+    status = mm_read_mtx_crd_size(mmfile, m, n, nnz);
+    assert(status == 0 && "Parsed matrix m, n, and nnz.");
+    printf("- matrix is %d-by-%d with %d nnz.\n", *m, *n, *nnz);
+
+    int *coo_rows = (int*) malloc(*nnz * sizeof(int));
+    int *coo_cols = (int*) malloc(*nnz * sizeof(int));
+    float *coo_vals = (float*) malloc(*nnz * sizeof(float));
+
+    printf("reading COO matrix file\n");
+    for (int i = 0; i < *nnz; i++)
+        status = fscanf(mmfile, "%d %d %g\n", &coo_rows[i], &coo_cols[i], &coo_vals[i]);
+
+    *rowptrs = (int*) malloc((*m+1)*sizeof(int));
+    *colinds = (int*) malloc(*nnz*sizeof(int));
+    *vals = (float*) malloc(*nnz*sizeof(int));
+
+    printf("converting to CSR matrix\n");
+    int info;
+    int job[] = {
+        2, // job(1)=2 (coo->csr with sorting)
+        1, // job(2)=1 (one-based indexing for csr matrix)
+        1, // job(3)=1 (one-based indexing for coo matrix)
+        0, // empty
+        *nnz, // job(5)=nnz (sets nnz for csr matrix)
+        0  // job(6)=0 (all output arrays filled)
+    };
+    mkl_scsrcoo(job, m, *vals, *colinds, *rowptrs, nnz, coo_vals, coo_rows, coo_cols, &info);
+    assert(info == 0 && "Converted COO->CSR");
+
+    free(coo_rows);
+    free(coo_cols);
+    free(coo_vals);
+
+    printf("done parsing matrix\n");
+}
+
+void
+check_vec(int n, float *expected, float *actual) {
+    int errors = 0;
+    for(int i = 0; i < n; i++)
+        if ( fabs(expected[i] - actual[i]) > 2.0*FLT_EPSILON )
+            errors += 1;
+
+    fprintf(stderr, "WARNING: Found %d/%d errors in answer.\n", errors, n);
+}
+
+float *
+randvec(int n) {
+    float *v = (float*) malloc(n * sizeof(float));
+    for(int i = 0; i < n; i++)
+        v[i] = rand() / (float) RAND_MAX;
+    return v;
+}
+
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s MATRIX.mm\n", argv[0]);
+        exit(1);
+    }
+
+    int m, n, nnz;
+    int *rowptrs, *colinds;
+    float *vals;
+
+    printf("reading matrix at %s\n", argv[1]);
+    mm_read(argv[1], &m, &n, &nnz, &rowptrs, &colinds, &vals);
+
+    printf("building vector\n");
+    float *v = randvec(n);
+    float *cpu_answer = (float*) malloc(m*sizeof(float)),
+          *mic_answer = (float*) malloc(m*sizeof(float));
+
+    struct timeval start, end;
+
+    printf("running mkl-host tests\n");
+    double cpuAvgTimeInSec = 0.0;
+    for(int i = 0; i < NITER; i++) {
+        gettimeofday (&start, NULL);
+        {
+            mkl_scsrgemv((char*)"N", &m, vals, rowptrs, colinds, v, cpu_answer);
+        }
+        gettimeofday (&end, NULL);
+        cpuAvgTimeInSec += (end.tv_sec-start.tv_sec) + 1.e-6*(end.tv_usec - start.tv_usec);
+    }
+    cpuAvgTimeInSec /= (float) NITER;
+    
+
+    printf("running mkl-mic tests\n");
+    double micAvgTimeInSec = 0.0;
+    for(int i = 0; i < NITER; i++) {
+        gettimeofday (&start, NULL);
+        {
+            mkl_scsrgemv((char*)"N", &m, vals, rowptrs, colinds, v, mic_answer);
+        }
+        gettimeofday (&end, NULL);
+        micAvgTimeInSec += (end.tv_sec-start.tv_sec) + 1.e-6*(end.tv_usec - start.tv_usec);
+    }
+    micAvgTimeInSec /= (float) NITER;
+
+    printf("checking the answer\n");
+    check_vec(m, cpu_answer, mic_answer);
+
+    double gflop = 2.e-9 * 2.0 * nnz;
+    double gbytes = 2.e-9 * (
+            nnz * sizeof(float) + // vals
+            nnz * sizeof(int) + // cols
+            m * sizeof(int) + // rows
+            (n+m) * sizeof(float)); // vectors
+
+    printf("Platform  Time         Gflops/s    %%peak Gbytes/s     %%peak\n");
+    printf("MKL-host % 1.8f  % 2.8f  %02.f   %02.8f   %02.f\n", cpuAvgTimeInSec,
+            gflop/cpuAvgTimeInSec, 100.0*gflop/cpuAvgTimeInSec/3.33/6.0,
+            gbytes/cpuAvgTimeInSec, 100.0*gbytes/cpuAvgTimeInSec/32.0);
+    printf("MKL-mic  % 1.8f  % 2.8f  %02.f   %02.8f   %02.f\n", micAvgTimeInSec,
+            gflop/micAvgTimeInSec, 100.0*gflop/micAvgTimeInSec/1.053/60.0,
+            gbytes/micAvgTimeInSec, 100.0*gbytes/micAvgTimeInSec/320.0);
+
+    return 0;
+}
